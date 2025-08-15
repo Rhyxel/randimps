38b676de02366f06cb7841bc430d40739fabc9f0
Rhyxel <aithne42@gmail.com>
2025-08-14 20:58:54 -0400

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -15,9 +15,9 @@ let charadex = {};
 /* Any preview links will still show Charadex's information
 /* ==================================================================== */
 charadex.site = {
-  title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
-  description: `A tool for organizing small ARPGs and species.`
+  title: "Randimps",
+  url: "https://rhyxel.github.io/randimps/",
+  description: `The Randimps masterlist and inventories`
 }
 
 /* ==================================================================== */
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1jW-eLKst9lRbFb8I_zV_oxYWiNrLSzia-BvZ_C4_dOI",
 
   pages: {
     masterlist:    "masterlist",
@@ -42,12 +42,12 @@ charadex.sheet = {
 
   options: {
 
-    designTypes: ['All', 'Official Design', 'Guest Design', 'MYO Slot', 'MYO Design'],
-    statuses: ['All', 'Resell', 'Trade', 'Gift', 'Voided', 'For Sale', 'Purchased'],
-    rarity: ['All', 'Common', 'Uncommon', 'Rare', 'Very Rare', 'Legendary'],
-    species: ['All', 'Dog', 'Cat', 'Bunny'],
-    itemTypes: ['All', 'Currency', 'MYO Slot', 'Pet', 'Trait', 'Misc'],
-    traitTypes: ['All', 'Ears', 'Eyes', 'Body', 'Limbs', 'Tails', 'Misc', 'Mutations']
+    designTypes: ['All', 'Gacha', 'Encounter', 'Custom', 'Event'],
+    statuses: ['All', 'Rollable', 'Owned'],
+    rarity: ['All', 'Common', 'Odd', 'Peculiar'],
+    species: ['All', 'Randimp'],
+    itemTypes: ['All', 'Currency', 'Bait'],
+    traitTypes: ['All', 'Ears', 'Feet', 'Horns', 'Eyes', 'Tail', 'Addons', 'Tendency']
 
   }
 
